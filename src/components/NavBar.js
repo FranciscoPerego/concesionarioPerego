39db363e24bc20837e0e8adea1fa3c0e405d2ae1
FranciscoPerego <francisco.perego@gmail.com>
2021-12-14 20:47:01 -0300

    modificacion final

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-export const NavBar = () => {
+export const navBar = () => {
     return (
-                <nav className="navBar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                <nav className="navBar navBar-expand-lg navBar-light fixed-top" id="mainNav">
                   <div className="container px-4 px-lg-5">
-                    <a className="navbar-brand" href="#page-top">PEREGO LUXURY CAR RENTALS</a>
-                    <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className="navBar-brand" href="#page-top">PEREGO LUXURY CAR RENTALS</a>
+                    <button className="navBar-toggler navBar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navBarResponsive" aria-controls="navBarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                       Menu
                       <i className="fas fa-bars" />
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarResponsive">
-                      <ul className="navbar-nav ms-auto">
+                    <div className="collapse navBar-collapse" id="navBarResponsive">
+                      <ul className="navBar-nav ms-auto">
                         <li className="nav-item"><a className="nav-link" href="#">Principal</a></li>
                         <li className="nav-item"><a className="nav-link" href="#">Servicios</a></li>
                         <li className="nav-item"><a className="nav-link" href="#">Marcas de autos</a></li>
@@ -23,4 +23,4 @@ export const NavBar = () => {
               );
             }
 
-export default NavBar
+export default navBar
